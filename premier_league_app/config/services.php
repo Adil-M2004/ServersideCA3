b3c483edc677ebb55631193bdabaf4138b2ca9b6
Adil-M2004 <D00265739@student.dkit.ie>
2025-05-08 08:55:16 +0100

    google login attempt

--- premier_league_app/config/services.php
+++ premier_league_app/config/services.php
@@ -35,4 +35,11 @@ return [
         ],
     ],
 
+    //GOOGLE LOGIN
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => "http://127.0.0.1:8000/auth/google/callback",
+    ],
+
 ];
